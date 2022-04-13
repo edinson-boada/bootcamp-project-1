bd55790ddf73aeb740494f82231b4c2b1e95dcea
paolo-boada-unmsm <edinson.boada@unmsm.edu.pe>
2022-04-13 14:58:29 -0500

    base project

---
+++ src/main/java/com/nttdata/project01/creditBank/CreditBankApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.project01.creditBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/project01/creditBank/CreditBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.project01.creditBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
