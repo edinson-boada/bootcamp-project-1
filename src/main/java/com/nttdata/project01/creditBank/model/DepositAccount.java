8c49ba535bd9925e80e9580f49add838ae6ad574
edinson-boada <edinson.boada@unmsm.edu.pe>
2022-04-17 16:12:26 -0500

    fix model

--- src/main/java/com/nttdata/project01/creditBank/model/DepositAccount.java
+++ src/main/java/com/nttdata/project01/creditBank/model/DepositAccount.java
@@ -2,15 +2,10 @@ package com.nttdata.project01.creditBank.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
-@Document(collection="deposit_accounts")
 public class DepositAccount {
-    @Id
-    private String id;
     private int movements;
     private int day;
 }
