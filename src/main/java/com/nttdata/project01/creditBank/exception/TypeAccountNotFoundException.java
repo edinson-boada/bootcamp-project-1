dd633276ad205f73b8691fe610d279109372382f
edinson-boada <edinson.boada@unmsm.edu.pe>
2022-04-25 17:37:15 -0500

    clean unused

--- src/main/java/com/nttdata/project01/creditBank/exception/TypeAccountNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.nttdata.project01.creditBank.exception;
-
-public class TypeAccountNotFoundException extends RuntimeException {
-	public TypeAccountNotFoundException(String message) {
-		super(message);
-	}
-}
